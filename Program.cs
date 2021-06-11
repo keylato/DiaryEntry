52e76b72547b3d296b42d21940536f59b85a48aa
keylato <78444133+keylato@users.noreply.github.com>
2021-06-10 21:41:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Globalization;
+
+namespace DairyEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DateTime hoy = DateTime.Now;
+            CultureInfo culture = new CultureInfo("es-Mx");
+            string hoyAsStr = hoy.ToString(culture);
+            string nombreDocumento = hoyAsStr.Replace("/","=");
+
+            Console.WriteLine("Entas entrando a tu Diario");
+            string bienvenida = Console.ReadLine();
+            Console.WriteLine("guardando la fecha de hoy" + hoyAsStr);
+
+            using(StreamWriter sw = new StreamWriter(nombreDocumento+".txt"))
+            {
+                sw.WriteLine(bienvenida);
+            }
+
+
+
+        }
+    }
+}
